0958c023d8a6c0cd2de14acb80a962c450fe0303
MilicTG <milic.tg@gmail.com>
2021-10-21 21:56:34 +0200

    Registering users

--- app/src/main/java/com/androiddevs/ktornoteapp/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.androiddevs.ktornoteapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.androiddevs.ktornoteapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
--- app/src/main/java/com/androiddevs/ktornoteapp/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/ui/auth/AuthFragment.kt
@@ -1,16 +1,23 @@
 package com.androiddevs.ktornoteapp.ui.auth
 
+import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.androiddevs.ktornoteapp.databinding.FragmentAuthBinding
+import com.androiddevs.ktornoteapp.other.Status
 import com.androiddevs.ktornoteapp.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AuthFragment : BaseFragment() {
 
     private lateinit var binding: FragmentAuthBinding
+
+    private val viewModel: AuthViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,11 +30,42 @@ class AuthFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnLogin.setOnClickListener {
-            findNavController().navigate(
-                AuthFragmentDirections.actionAuthFragmentToNotesFragment()
-            )
+        requireActivity().requestedOrientation = SCREEN_ORIENTATION_PORTRAIT
+
+        subscribeToObservers()
+
+        binding.btnRegister.setOnClickListener {
+            val email = binding.etRegisterEmail.text.toString()
+            val password = binding.etRegisterPassword.text.toString()
+            val confirmedPassword = binding.etRegisterPasswordConfirm.text.toString()
+            viewModel.register(email, password, confirmedPassword)
         }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.registerStatus.observe(
+            viewLifecycleOwner, Observer { result ->
+                result?.let {
+                    when (result.status) {
+                        Status.SUCCESS -> {
+                            binding.registerProgressBar.visibility = View.GONE
+                            showSnackbar(
+                                result.data ?: "Successfully register an account!"
+                            )
+                        }
+                        Status.ERROR -> {
+                            binding.registerProgressBar.visibility = View.GONE
+                            showSnackbar(
+                                result.message ?: "A unknown error occurred!"
+                            )
+                        }
+                        Status.LOADING -> {
+                            binding.registerProgressBar.visibility = View.VISIBLE
+                        }
+                    }
+                }
+            }
+        )
     }
 
 }
