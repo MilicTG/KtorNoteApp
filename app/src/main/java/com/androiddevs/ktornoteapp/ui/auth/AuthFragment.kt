16f75d8160b63d4a5dadf066353e7e306e53e83b
MilicTG <milic.tg@gmail.com>
2021-10-23 20:24:59 +0200

    Logging in users

--- app/src/main/java/com/androiddevs/ktornoteapp/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/ui/auth/AuthFragment.kt
@@ -1,5 +1,6 @@
 package com.androiddevs.ktornoteapp.ui.auth
 
+import android.content.SharedPreferences
 import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,10 +8,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import com.androiddevs.ktornoteapp.R
+import com.androiddevs.ktornoteapp.data.remote.BasicAuthInterceptor
 import com.androiddevs.ktornoteapp.databinding.FragmentAuthBinding
+import com.androiddevs.ktornoteapp.other.Constants.KEY_LOGGED_IN_EMAIL
+import com.androiddevs.ktornoteapp.other.Constants.KEY_PASSWORD
 import com.androiddevs.ktornoteapp.other.Status
 import com.androiddevs.ktornoteapp.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class AuthFragment : BaseFragment() {
@@ -18,6 +26,15 @@ class AuthFragment : BaseFragment() {
     private lateinit var binding: FragmentAuthBinding
 
     private val viewModel: AuthViewModel by viewModels()
+
+    @Inject
+    lateinit var sharedPref: SharedPreferences
+
+    @Inject
+    lateinit var basicAuthInterceptor: BasicAuthInterceptor
+
+    private var currentEmail: String? = null
+    private var currentPassword: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,11 +55,81 @@ class AuthFragment : BaseFragment() {
             val email = binding.etRegisterEmail.text.toString()
             val password = binding.etRegisterPassword.text.toString()
             val confirmedPassword = binding.etRegisterPasswordConfirm.text.toString()
-            viewModel.register(email, password, confirmedPassword)
+            viewModel.register(
+                email = email,
+                password = password,
+                repeatedPassword = confirmedPassword
+            )
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etLoginEmail.text.toString()
+            val password = binding.etLoginPassword.text.toString()
+            currentEmail = email
+            currentPassword = password
+            viewModel.login(
+                email = email,
+                password = password
+            )
         }
     }
 
+    private fun authenticateApi(email: String, password: String) {
+        basicAuthInterceptor.email = email
+        basicAuthInterceptor.password = password
+    }
+
+    private fun redirectLogin() {
+        val navOptions = NavOptions.Builder()
+            .setPopUpTo(R.id.authFragment, true)
+            .build()
+        findNavController().navigate(
+            AuthFragmentDirections.actionAuthFragmentToNotesFragment(),
+            navOptions
+        )
+    }
+
     private fun subscribeToObservers() {
+
+        viewModel.loginStatus.observe(
+            viewLifecycleOwner, Observer { result ->
+                result?.let {
+                    when (result.status) {
+                        Status.SUCCESS -> {
+                            binding.loginProgressBar.visibility = View.GONE
+                            showSnackbar(
+                                text = result.data ?: "Successfully logged in"
+                            )
+
+                            sharedPref.edit().putString(
+                                KEY_LOGGED_IN_EMAIL, currentEmail
+                            ).apply()
+                            sharedPref.edit().putString(
+                                KEY_PASSWORD, currentPassword
+                            ).apply()
+
+                            authenticateApi(
+                                email = currentEmail ?: "",
+                                password = currentPassword ?: ""
+                            )
+
+                            redirectLogin()
+                        }
+                        Status.ERROR -> {
+                            binding.loginProgressBar.visibility = View.GONE
+                            showSnackbar(
+                                text = result.message ?: "A unknown error accrued!"
+                            )
+                        }
+                        Status.LOADING -> {
+                            binding.loginProgressBar.visibility = View.VISIBLE
+                        }
+                    }
+                }
+            }
+        )
+
+
         viewModel.registerStatus.observe(
             viewLifecycleOwner, Observer { result ->
                 result?.let {
@@ -50,13 +137,13 @@ class AuthFragment : BaseFragment() {
                         Status.SUCCESS -> {
                             binding.registerProgressBar.visibility = View.GONE
                             showSnackbar(
-                                result.data ?: "Successfully register an account!"
+                                text = result.data ?: "Successfully register an account!"
                             )
                         }
                         Status.ERROR -> {
                             binding.registerProgressBar.visibility = View.GONE
                             showSnackbar(
-                                result.message ?: "A unknown error occurred!"
+                                text = result.message ?: "A unknown error occurred!"
                             )
                         }
                         Status.LOADING -> {
@@ -67,5 +154,4 @@ class AuthFragment : BaseFragment() {
             }
         )
     }
-
 }
