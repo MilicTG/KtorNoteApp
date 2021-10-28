9dde086e92ce6f995c1f489a214ad97f984e046b
MilicTG <milic.tg@gmail.com>
2021-10-28 21:24:41 +0200

    Implementing the caching mechanism

--- app/src/main/java/com/androiddevs/ktornoteapp/repositories/NoteRepository.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/repositories/NoteRepository.kt
@@ -2,10 +2,14 @@ package com.androiddevs.ktornoteapp.repositories
 
 import android.app.Application
 import com.androiddevs.ktornoteapp.data.local.NoteDao
+import com.androiddevs.ktornoteapp.data.local.entitys.Note
 import com.androiddevs.ktornoteapp.data.remote.NoteApi
 import com.androiddevs.ktornoteapp.data.remote.requests.AccountRequest
 import com.androiddevs.ktornoteapp.other.Resource
+import com.androiddevs.ktornoteapp.other.checkForInternetConnection
+import com.androiddevs.ktornoteapp.other.networkBoundResource
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import java.lang.Exception
 import javax.inject.Inject
@@ -15,6 +19,26 @@ class NoteRepository @Inject constructor(
     private val noteApi: NoteApi,
     private val context: Application
 ) {
+
+    fun getAllNotes(): Flow<Resource<List<Note>>> {
+        return networkBoundResource(
+            query = {
+                noteDao.getAllNotes()
+            },
+            fetch = {
+                noteApi.getNotes()
+            },
+            saveFetchResult = { response ->
+                response.body()?.let {
+                    //TODO: insert notes in database
+
+                }
+            },
+            shouldFetch = {
+                checkForInternetConnection(context = context)
+            }
+        )
+    }
 
     suspend fun login(email: String, password: String) = withContext(Dispatchers.IO) {
         try {
